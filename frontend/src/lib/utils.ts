7fe7891ea6d3f90af0ba54e2bcc6d5c063680beb
German <7802598+germanllop@users.noreply.github.com>
2025-11-07 23:51:07 -0500

    fix: [frontend] Determine wallet address length for validation

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,6 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+const EXPECTED_WALLET_ADDRESS_LENGTH = 42;
+
 export function shortWalletAddress(address: string) {
+  if (address.length !== EXPECTED_WALLET_ADDRESS_LENGTH) {
+    throw new Error(
+      `Wallet address must be ${EXPECTED_WALLET_ADDRESS_LENGTH} characters long`
+    );
+  }
+
   return `${address.slice(0, 6)}...${address.slice(-4)}`
 }
